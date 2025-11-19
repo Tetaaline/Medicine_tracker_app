3040788bcb451c973ff59aa365b18072fd0ff928
aline <a.teta2@alustudent.com>
2025-11-19 07:33:40 -0800

    Validators files update

--- src/interface/validators.py
+++ src/interface/validators.py
@@ -11,3 +11,11 @@ def is_alnum_username(text: str) -> bool:
 
 def is_valid_dosage(text: str) -> bool:
     return bool(re.fullmatch(r"\d+(\.\d+)?\s?(mg|g|l)", text, flags=re.IGNORECASE))
+
+def is_valid_time_hms(text: str) -> bool:
+    try:
+        datetime.strptime(text, "%H:%M:%S")
+        return bool(re.fullmatch(r"\d{2}:\d{2}:\d{2}", text))
+    except ValueError:
+        return False
+
