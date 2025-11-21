737989d7f95d50a03315faa223a43791c3dc04f9
aline <a.teta2@alustudent.com>
2025-11-21 04:56:09 -0800

    Menu.py file renewed version

--- src/interface/menu.py
+++ src/interface/menu.py
@@ -1,6 +1,6 @@
 #Importing colorama for colored output in the terminal
 from colorama import Fore, Style
-
+#Function to print colored headerin the terminal
 def print_header(title):
     print('\n' + Fore.LIGHTBLUE_EX + '='*60 + Style.RESET_ALL)
     print(Fore.LIGHTYELLOW_EX + title + Style.RESET_ALL)
